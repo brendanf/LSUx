c54dff505a95cea100b385072b11a508eab6f7d8
brendanf <brendan.furneaux@gmail.com>
2020-04-24 15:25:14 +0200

    update tests for coercion to use as() instead of as_sread().

--- tests/testthat/test-sread.R
+++ tests/testthat/test-sread.R
@@ -2,9 +2,9 @@ test_file <- inferrnal::sample_rRNA_fasta()
 test_sread <- ShortRead::readFasta(test_file)
 test_DNAss <- Biostrings::readDNAStringSet(test_file)
 
-sread_file <- as_sread(test_file)
-sread_sread <- as_sread(test_sread)
-sread_DNAss <- as_sread(test_DNAss)
+sread_file <- methods::as(test_file, "ShortRead")
+sread_sread <- methods::as(test_sread, "ShortRead")
+sread_DNAss <- methods::as(test_DNAss, "ShortRead")
 test_that("loading fasta by Biostrings and ShortRead yield same result from as_sread", {
   expect_equal(sread_file, sread_sread)
   expect_equal(sread_file, sread_DNAss)
